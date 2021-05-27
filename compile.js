3e8a919034598d93c60643e63d43dce639d783b3
Pallavi Saxena <43824903+pallavisaxena215@users.noreply.github.com>
2021-05-27 14:47:00 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -2,11 +2,11 @@ const path = require('path');// will hep in building a path from compile.js file
 const fs = require ('fs');// to read the contents of the file we use file system (fs) module
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname,'contracts', 'Inbox.sol'); //declaration of path 
+const LotteryPath = path.resolve(__dirname,'contracts', 'Lottery.sol'); //declaration of path 
 
 
-const source= fs.readFileSync(inboxPath,'utf8');
+const source= fs.readFileSync(LotteryPath,'utf8');
 
-module.exports=solc.compile(source,1).contracts[':Inbox'];
+module.exports=solc.compile(source,1).contracts[':Lottery'];
  // the exports statement will make the byte code of the contract to all the other files
 
